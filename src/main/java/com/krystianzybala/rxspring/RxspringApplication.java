c779e9abf389c525a72d8d5d5b408bbadab2bd5f
Krystian Zybala <kontakt@kzybala.pl>
2019-03-25 18:45:05 +0100

    Enable webflux

--- src/main/java/com/krystianzybala/rxspring/RxspringApplication.java
+++ src/main/java/com/krystianzybala/rxspring/RxspringApplication.java
@@ -2,8 +2,10 @@ package com.krystianzybala.rxspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class RxspringApplication {
 
     public static void main(String[] args) {
