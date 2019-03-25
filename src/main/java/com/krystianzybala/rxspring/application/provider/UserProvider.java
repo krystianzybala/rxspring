d1b47426f258564920ed340d3417caeb6966a4ea
Krystian Zybala <kontakt@kzybala.pl>
2019-03-25 18:57:34 +0100

    Add simple implementation for "unbounded" stream..

--- src/main/java/com/krystianzybala/rxspring/application/provider/UserProvider.java
+++ src/main/java/com/krystianzybala/rxspring/application/provider/UserProvider.java
@@ -3,22 +3,28 @@ package com.krystianzybala.rxspring.application.provider;
 import com.krystianzybala.rxspring.application.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.UUID;
+import java.util.Arrays;
 
 @Slf4j
 @Service
 public class UserProvider {
 
-    public void getUsers() {
-
+    Flux<User> getUsers() {
+        log.info("Get users");
+        return Mono.fromCallable(() -> Arrays.asList("Tom", "Joe", "George", "Harry", "Ron"))
+                .flux()
+                .concatMap(Flux::fromIterable)
+                .map(User::new);
     }
 
 
-    public Mono<User> getUserByName(final String name) {
+    Mono<User> loadUserByName(final String name) {
 
+        log.info("Load user by name: {}", name);
         return Mono
                 .fromCallable(() -> new User("Joe"))
                 .delaySubscription(Duration.ofSeconds(10));
