c3c94cf04cfb281040b2b0120742c87f9e483dd5
Krystian Zybala <kontakt@kzybala.pl>
2019-03-25 17:47:29 +0100

    base project

---
+++ src/main/java/com/krystianzybala/rxspring/RxspringApplication.java
@@ -0,0 +1,13 @@
+package com.krystianzybala.rxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krystianzybala/rxspring/RxspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krystianzybala.rxspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
